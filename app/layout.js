dec6f6e2140ef1cc0ab51c944857d48d771231d0
jadojadox01 <niyomuhozajeandedieu80@gmail.com>
2026-01-16 15:05:17 +0200

    PDF TO WORD APP

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Script from "next/script";
 
 export const metadata = {
   title: "Free PDF iPhone Tools - Convert PDF to Word & JPG",
@@ -9,6 +10,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-Y7LNYKSGJH"
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Y7LNYKSGJH');
+            `,
+          }}
+        />
+      </head>
       <body style={{ margin: 0, fontFamily: "Arial, sans-serif" }}>
         <Navbar />
         {children}
